f05a72c4b81215dd8de343a95822673af0d57a29
Jonathan <jsanch88@stu.southtexascollege.edu>
2021-11-10 21:42:51 -0600

    Basic Setup

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -13,31 +13,31 @@ const loggingOut = () => {
 </script>
 
 <template>
-<div class="bg-purple-500 text-white">
+<div class="bg-red-500 text-black">
 <div class = "container mx-auto flex items-center justify-between">
 
 <h1 class="tracking-tighter text-3xl font-bold">
-Vue<span class="bg bg-pink-300 font-normal">FakeAuth</span>
+Vue<span class="bg bg-indigo-600 font-normal">RealAuth</span>
 </h1>
     
     <nav>
         <ul class= "flex space-x-4">
         <router-link to = "/">
-            <li class="bg-pink-500 py-8 px-4 hover: cursor-pointer hover:bg-gray-300 hover:text-blue-700">Home</li></router-link>
+            <li class="bg-yellow-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">Home</li></router-link>
 
             <router-link  to = "/about">
-            <li class="bg-pink-500 py-8 px-4 hover: cursor-pointer hover:bg-gray-300 hover:text-blue-700">About</li></router-link>
+            <li class="bg-yellow-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">About</li></router-link>
 
             <router-link v-if = "!isAuthenticated" to = "/login">
-            <li class="bg-pink-500 py-8 px-4 hover: cursor-pointer hover:bg-gray-300 hover:text-blue-700">Login</li></router-link>
+            <li class="bg-yellow-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">Login</li></router-link>
 
             <div v-else class = "flex">
 
             <router-link to = "/secret">
-            <li class="bg-pink-500 py-8 px-4 hover: cursor-pointer hover:bg-gray-300 hover:text-blue-700">Secret</li></router-link>
+            <li class="bg-yellow-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">Secret</li></router-link>
 
             <button  @click = "loggingOut">
-            <li class="bg-pink-500 py-8 px-4 hover: cursor-pointer hover:bg-gray-300 hover:text-blue-700">Logout</li></button>
+            <li class="bg-yellow-400 py-8 px-4 hover: cursor-pointer hover:bg-purple-900 hover:text-pink-700">Logout</li></button>
 
             </div>
 
